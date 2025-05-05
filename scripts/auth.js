70fa22d07c7b6c21c771ada7df9fe66cc40b231c
Aoua <aoua.sow1@gmail.com>
2025-05-05 17:22:22 -0400

    update

--- scripts/auth.js
+++ scripts/auth.js
@@ -6,7 +6,7 @@ class Auth {
     }
     validateAuth(auth) {
         if(auth != 1) {
-            window.location.replace('courseNodeFrontend/login.html')
+            window.location.replace('/courseNodeFrontend/login.html')
         }
         else {
             document.querySelector('body').style.display = 'block'
